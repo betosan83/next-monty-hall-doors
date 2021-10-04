39a11aff4120f8751138e327f18dd883ea8fa050
Roberto Boscolo <roberto.bfigueredo@dataprev.gov.br>
2021-10-04 14:03:42 -0300

    Door component finished!

--- components/Door.jsx
+++ components/Door.jsx
@@ -6,9 +6,11 @@ export default function Door(props) {
         <div className={styles.area}>
             <div className={styles.structure}>
                 <div className={styles.door}>
-                    <div className={styles.numero}>3</div>
+                    <div className={styles.number}>3</div>
+                    <div className={styles.doorhandle}></div>
                 </div>
             </div>
+            <div className={styles.floor}></div>
         </div>
 
     )
