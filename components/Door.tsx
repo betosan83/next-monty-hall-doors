7baf1ddf0b869560f4ce68292380048924a172b9
Roberto Boscolo <roberto.bfigueredo@dataprev.gov.br>
2021-10-04 19:47:16 -0300

    Door opening!

--- components/Door.tsx
+++ components/Door.tsx
@@ -1,5 +1,5 @@
 import styles from "../styles/Door.module.css"
-import DoorModel from "../model/porta"
+import DoorModel from "../model/door"
 
 interface DoorProps {
     value: DoorModel
@@ -8,17 +8,28 @@ interface DoorProps {
 
 export default function Door(props: DoorProps) {
     const door = props.value
-    const selected = door.selected ? styles.selected : ''
+    const selected = door.selected && !door.open ? styles.selected : ''
 
     const toggleSelection = e => props.onChange(door.toggleSelection())
+    const open = e => {
+        e.stopPropagation()
+        props.onChange(door.openning())
+    }
+    function renderDoor() {
+        return (
+
+            <div className={styles.door}>
+                <div className={styles.number}>{door.number}</div>
+                <div className={styles.doorhandle} onClick={open}></div>
+            </div>
+
+        )
+    }
 
     return (
         <div className={styles.area} onClick={toggleSelection}>
             <div className={`${styles.structure} ${selected} `}>
-                <div className={styles.door}>
-                    <div className={styles.number}>{door.number}</div>
-                    <div className={styles.doorhandle}></div>
-                </div>
+                {door.open ? false : renderDoor()}
             </div>
             <div className={styles.floor}></div>
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Door from "../components/Door";
-import DoorModel from "../model/porta";
+import DoorModel from "../model/door";
 
 export default function Home() {
   const [d1, setD1] = useState(new DoorModel(1))
