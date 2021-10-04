8b6cfeab33b02b409eebb8e721565816b6d08346
Roberto Boscolo <roberto.bfigueredo@dataprev.gov.br>
2021-10-04 16:10:43 -0300

    Door model.

--- components/Door.jsx
+++ components/Door.jsx
@@ -1,10 +1,11 @@
 import styles from "../styles/Door.module.css"
 
 export default function Door(props) {
+    const selected = props.selected ? styles.selected : ''
 
     return (
         <div className={styles.area}>
-            <div className={styles.structure}>
+            <div className={`${styles.structure} ${selected} `}>
                 <div className={styles.door}>
                     <div className={styles.number}>3</div>
                     <div className={styles.doorhandle}></div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,9 +3,9 @@ import Gift from "../components/Gift";
 
 export default function Home() {
   return (
-    <div>
-      <Door></Door>
-      <Door></Door>
+    <div style={{display: "flex"}}>
+      <Door selected={true}></Door>
+      <Door selected={false}></Door>
     </div>
   )
 }
